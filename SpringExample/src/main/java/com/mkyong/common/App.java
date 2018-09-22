e6985481d603e47fcfec2b6ae0e5dff00ee29e88
marora224 <m.arora224@gmail.com>
2018-09-22 16:08:18 -0400

    Update App.java

--- SpringExample/src/main/java/com/mkyong/common/App.java
+++ SpringExample/src/main/java/com/mkyong/common/App.java
@@ -15,6 +15,6 @@ public class App
     	 // sysout testing 123
         HelloWorld obj = (HelloWorld) context.getBean("helloBean123");
         obj.printHello();
-        // push and commit
+        // push and commit 12
     }
 }
