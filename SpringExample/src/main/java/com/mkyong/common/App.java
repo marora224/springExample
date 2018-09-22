2ef10f84f74e3c108473e8deedfa49dc634a31cb
marora224 <m.arora224@gmail.com>
2018-09-22 15:14:04 -0400

    second commit

--- SpringExample/src/main/java/com/mkyong/common/App.java
+++ SpringExample/src/main/java/com/mkyong/common/App.java
@@ -12,7 +12,7 @@ public class App
     public static void main( String[] args )
     {
     	ApplicationContext context = new ClassPathXmlApplicationContext("Spring-Module.xml");
-    	 // sysout testing
+    	 // sysout testing 123
         HelloWorld obj = (HelloWorld) context.getBean("helloBean123");
         obj.printHello();
         
